460f96d103a7574735d73707d1e5b0b4916a951b
soojung <tnwjd18412@tw.ac.kr>
2021-06-10 22:42:25 +0900

    update

--- photo/views.py
+++ photo/views.py
@@ -7,12 +7,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-@login_required()
+@login_required
 def photo_list(request):
     photos = Photo.objects.all()
     return render(request,'photo/list.html',{'photos':photos})
 
-class PhotoUploadView(CreateView):
+class PhotoUploadView(LoginRequiredMixin,CreateView):
     model = Photo
     fields = ['photo','text']
     template_name = 'photo/upload.html'
@@ -25,16 +25,16 @@ class PhotoUploadView(CreateView):
         else:
             return self.render_to_response({'form':form})
 
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(LoginRequiredMixin,DeleteView):
     model = Photo
     success_url = '/'
     template_name = 'photo/delete.html'
 
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(LoginRequiredMixin,UpdateView):
     model = Photo
     fields = ['photo','text']
     template_name = 'photo/update.html'
 
-class PhotoDetailView(DeleteView):
+class PhotoDetailView(DetailView):
     model = Photo
     template_name = 'photo/detail.html'
